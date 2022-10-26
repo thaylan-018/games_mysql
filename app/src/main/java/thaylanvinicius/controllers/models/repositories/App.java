c66b7e635b379d24c6cce81a83a7cc0b16adda5d
thaylan-018 <thaylan.santos01@etec.sp.gov.br>
2022-10-26 17:33:03 +0000

    Estrutura

---
+++ app/src/main/java/thaylanvinicius/controllers/models/repositories/App.java
@@ -0,0 +1,11 @@
+package thaylanvinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
